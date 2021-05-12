6f4be8a184232bca94e9716837b18e4171f95503
KTreaster04 <79948666+KTreaster04@users.noreply.github.com>
2021-05-12 13:53:46 -0400

    Added the GitHub repo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>
+  return (
+  <div className="App">
+    <h1>Weather App</h1>
+    
+    <footer>
+      Project is created by Kaitlyn Treaster and is {" "}
+      <a
+      href="https://github.com/kaitlyn-treaster/react-weather-app"
+      target="_blank"
+      >
+        open-sourced on GitHub
+      </a>
+    </footer>
+  </div>
+  );
 }
 
 
