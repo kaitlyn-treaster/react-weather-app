e9cdb5fd72a825e82988182edd7fcb6f23de7a22
KTreaster04 <79948666+KTreaster04@users.noreply.github.com>
2021-05-12 14:16:34 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import "./App.css";
 export default function App() {
   return (
   <div className="App">
+    <div className="container">
     <h1>Weather App</h1>
-    
     <footer>
       Project is created by Kaitlyn Treaster and is {" "}
       <a
       href="https://github.com/kaitlyn-treaster/react-weather-app"
       target="_blank"
+      rel="noreferrer"
       >
         open-sourced on GitHub
       </a>
     </footer>
+    </div>
   </div>
   );
 }
